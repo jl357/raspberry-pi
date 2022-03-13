c6f0efb78391cbae4bd713b9bbaa273534d61619
jl357 <55468569+jl357@users.noreply.github.com>
2022-03-13 14:36:31 -0400

    added comments

--- first_project.py
+++ first_project.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setmode(GPIO.BOARD) # set mode to use pin numbers
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) # default state is 1, when pressed it will become 0
 
 
-while True:
-    if GPIO.input(18) == GPIO.HIGH:
-        print("You pressed the button!!!")
+while True: # this is an infinite loop
+    if GPIO.input(18) == GPIO.LOW: # == False == 0
+        print("You pressed the button!!!") # the button was pressed!
