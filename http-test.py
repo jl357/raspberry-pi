e1cfe3325f69999233e9279c19561bc5fdc88285
hail12pink <59028187+hail12pink@users.noreply.github.com>
2022-03-26 18:07:58 -0400

    Create http-test.py

---
+++ http-test.py
@@ -0,0 +1,21 @@
+from gpiozero import led
+from signal import pause
+import requests
+
+green = gpio.LED("BOARD8")
+yellow = gpio.LED("BOARD10")
+red = gpio.LED("BOARD12")
+
+yellow.on()
+
+response = requests.get("http://www.google.com")
+
+yellow.off()
+
+print(response)
+print(response.status_code)
+
+if response.status_code == 400:
+    green.on()
+else:
+    red.on()
