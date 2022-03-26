7ea5e3b9d4351eed2764df659c786b72fc1d5b0a
hail12pink <59028187+hail12pink@users.noreply.github.com>
2022-03-25 20:42:41 -0400

    Create stoplight_button.py

---
+++ stoplight_button.py
@@ -0,0 +1,34 @@
+# A stoplight that changes which light is enabled when the button is pressed
+
+import gpiozero as gpio
+from signal import pause
+
+button = gpio.Button(3)
+curLightPin = 8
+
+min = 8
+max = 12
+
+lights = [
+    gpio.GPIODevice(8),
+    gpio.GPIODevice(10),
+    gpio.GPIODevice(12),
+]
+
+def nextLight():
+    for light in lights:
+        light.off()
+
+    if curLightPin + 2 > max:
+        curLightPin = min
+    else:
+        curLightPin += 2
+    
+    gpio.GPIODevice(curLightPin).on()
+
+
+# lambda is basically just a one-line function
+button.when_pressed = nextLight
+
+# A pause is here so that way the script doesn't immediately exit; the above code won't do anything if the script exits
+pause()
